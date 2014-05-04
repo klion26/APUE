b5e83de6333322df083a77dcd4466b76f4771475
klion26 <qcx978132955@gmail.com>
2014-05-04 11:38:59 +0800

    finish chapter 15

---
+++ ch15/15-1.c
@@ -0,0 +1,32 @@
+#include "apue.h"
+
+int main(void)
+{
+	int n;
+	int fd[2];
+	pid_t pid;
+
+	char line[MAXLINE];
+
+	if(pipe(fd) < 0)
+		err_sys("pipe error");
+	if((pid = fork()) < 0)
+	{
+		err_sys("fork error");
+	}
+	else if(pid > 0)
+	{
+		//close(fd[0]);
+		write(fd[1], "hello world\n", 12);
+		n = read(fd[0], line, MAXLINE);
+		write(STDOUT_FILENO, line, n);
+	}
+	else
+	{
+		//close(fd[1]);
+		write(fd[1], "fuck shit\n", 10);
+		n = read(fd[0], line, MAXLINE);
+		write(STDOUT_FILENO, line, n);
+	}
+	exit(0);
+}
